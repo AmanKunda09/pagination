c4c8de147703eeb4ae8cb77e324c41f6647066eb
Aman <amankunda9@gmail.com>
2022-12-14 18:01:08 +0530

    added card logic

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import Follower from "./Follower";
 import { useFetch } from "./useFetch";
 
 const Home = () => {
@@ -10,6 +11,14 @@ const Home = () => {
         <h1>{loading ? "loading..." : "pagination"}</h1>
         <div className="underline"></div>
       </div>
+      <section className="followers">
+        <div className="container">
+          {data.map((person) => {
+            return <Follower key={person.id} {...person} />;
+          })}
+        </div>
+        {!loading && <div className="btn-container">hello</div>}
+      </section>
     </main>
   );
 };
