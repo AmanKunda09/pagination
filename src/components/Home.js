73f24f5a3b147d5b2e5607b278ba1f4a4ca4e0fc
Aman <amankunda9@gmail.com>
2022-12-14 23:36:11 +0530

    adding page logic

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,9 +8,26 @@ const Home = () => {
   const [followers, setFollowers] = useState([]);
 
   useEffect(() => {
+    if (loading) return;
     setFollowers(data[page]);
-  }, [loading]);
-
+  }, [loading, page]);
+  const handlePage = (index) => {
+    setPage(index);
+  };
+  const nextPage = () => {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1;
+      if (nextPage > data.length - 1) nextPage = 0;
+      return nextPage;
+    });
+  };
+  const prevPage = () => {
+    setPage((oldPage) => {
+      let prevPage = oldPage - 1;
+      if (oldPage === 0) prevPage = data.length - 1;
+      return prevPage;
+    });
+  };
   return (
     <main>
       <div className="section-title">
@@ -23,7 +40,27 @@ const Home = () => {
             return <Follower key={person.id} {...person} />;
           })}
         </div>
-        {!loading && <div className="btn-container">hello</div>}
+        {!loading && (
+          <div className="btn-container">
+            <button className="prev-btn" onClick={prevPage}>
+              prev
+            </button>
+            {data.map((item, index) => {
+              return (
+                <button
+                  key={index}
+                  className={`page-btn ${index === page && "active-btn"}`}
+                  onClick={() => handlePage(index)}
+                >
+                  {index + 1}
+                </button>
+              );
+            })}
+            <button className="prev-btn" onClick={nextPage}>
+              next
+            </button>
+          </div>
+        )}
       </section>
     </main>
   );
